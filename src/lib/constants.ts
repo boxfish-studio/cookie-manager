e98d4a810b5e39ad1bb01addb04182502396f745
Begoña Alvarez <balvarez@boxfish.studio>
2022-02-16 20:42:34 +0100

    move constant to file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,10 @@
+import { SuportedService } from './types';
+
 export const COOKIE_EXPIRATION_DAYS = 30;
+
 export const COOKIE_NAME_PREFIX = 'skcm-';
+
+export const SupportedNecessaryCookies = {
+    [SuportedService.GoogleAnalyticsUniversal]: `${COOKIE_NAME_PREFIX}-ga-universal`,
+    [SuportedService.GoogleAnalytics4]: `${COOKIE_NAME_PREFIX}-ga-4`,
+}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,6 @@
-import { COOKIE_NAME_PREFIX } from './constants';
-
 export enum SuportedService {
     GoogleAnalyticsUniversal = 'googleAnalyticsUniversal',
     GoogleAnalytics4 = 'googleAnalytics4',
-}
-
-export const SupportedNecessaryCookies = {
-    [SuportedService.GoogleAnalyticsUniversal]: `${COOKIE_NAME_PREFIX}-ga-universal`,
-    [SuportedService.GoogleAnalytics4]: `${COOKIE_NAME_PREFIX}-ga-4`,
 }
 
 export type Service = {
