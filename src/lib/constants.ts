48b1bf64a6647a5bd233528bb4a2dc11505d05a1
Diego <zitvura@gmail.com>
2022-03-07 16:11:40 +0100

    make constant variable uppercase

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@ export const COOKIE_EXPIRATION_DAYS = 30;
 
 export const COOKIE_NAME_PREFIX = 'skcm-';
 
-export const NecessaryCookies = {
+export const NECESSARY_COOKIES = {
     [SupportedService.GoogleAnalyticsUniversal]: `${COOKIE_NAME_PREFIX}-ga-universal`,
     [SupportedService.GoogleAnalytics4]: `${COOKIE_NAME_PREFIX}-ga-4`,
 }
