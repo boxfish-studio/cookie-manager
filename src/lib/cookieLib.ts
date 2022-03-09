bd2fc13b32a3085878b4f10c5e077227f25baa5d
Diego <zitvura@gmail.com>
2022-03-09 14:15:09 +0100

    set enum variants in pascal case

--- src/lib/cookieLib.ts
+++ src/lib/cookieLib.ts
@@ -1,19 +1,20 @@
 import type { ServiceCookie } from './types';
 
 export enum Provider {
-    google = "Google"
+    Google = "Google"
 }
 export enum PrivacyUrl {
-    google = 'https://policies.google.com/privacy'
+    Google = 'https://policies.google.com/privacy'
 }
+
 export enum CookieType {
     HTTP = 'HTTP'
 }
 
 export const GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_ga',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
     expiry: '2 years',
     type: CookieType.HTTP,
@@ -21,8 +22,8 @@ export const GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
 
 export const GAT_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_gat',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: 'Used by Google Analytics to throttle request rate.',
     expiry: '1 day',
     type: CookieType.HTTP
@@ -30,8 +31,8 @@ export const GAT_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
 
 export const GID_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_gid',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
     expiry: '1 day',
     type: CookieType.HTTP
@@ -39,8 +40,8 @@ export const GID_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
 
 export const GAC_PROPERTY_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_gac_<property-id>',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: ' Contains campaign related information for the user. If you have linked your Google Analytics and Google Ads accounts, Google Ads website conversion tags will read this cookie unless you opt-out.',
     expiry: '90 days',
     type: CookieType.HTTP
@@ -48,8 +49,8 @@ export const GAC_PROPERTY_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
 
 export const GA_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
     name: '_ga',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
     expiry: '2 years',
     type: CookieType.HTTP
@@ -57,8 +58,8 @@ export const GA_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
 
 export const GA_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
     name: '_ga_<container-id>',
-    provider: Provider.google,
-    providerUrl: PrivacyUrl.google,
+    provider: Provider.Google,
+    providerUrl: PrivacyUrl.Google,
     purpose: 'Used to persist session state.',
     expiry: '2 years',
     type: CookieType.HTTP
