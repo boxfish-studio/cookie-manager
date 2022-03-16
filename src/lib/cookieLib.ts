ea80618ccc76545000dbe671d4ca2d87ee68cb45
Diego <zitvura@gmail.com>
2022-03-16 11:50:54 +0100

    update google cookies

--- src/lib/cookieLib.ts
+++ src/lib/cookieLib.ts
@@ -25,7 +25,7 @@ export const SKCM_GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     provider: Provider.Iota,
     providerUrl: RELEVANT_LINKS.Iota.privacy,
     purpose: 'Stores the user´s Google Analytics Universal cookies consent state for the current domain',
-    expiry: COOKIE_EXPIRATION_DAYS+ ' days',
+    expiry: COOKIE_EXPIRATION_DAYS + ' days',
     type: CookieType.HTTP,
 }
 
@@ -34,39 +34,48 @@ export const SKCM_GA_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
     provider: Provider.Iota,
     providerUrl: RELEVANT_LINKS.Iota.privacy,
     purpose: 'Stores the user´s Google Analytics 4 cookies consent state for the current domain',
-    expiry: COOKIE_EXPIRATION_DAYS+ ' days',
+    expiry: COOKIE_EXPIRATION_DAYS + ' days',
     type: CookieType.HTTP,
 }
- 
+
 export const NECESSARY_COOKIES = {
     [SupportedService.GoogleAnalyticsUniversal]: SKCM_GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE,
     [SupportedService.GoogleAnalytics4]: SKCM_GA_GOOGLE_ANALYTICS_4_COOKIE,
-} 
+}
 
 export const GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_ga',
     provider: Provider.Google,
     providerUrl: RELEVANT_LINKS.Google.privacy,
-    purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
+    purpose: 'Used to distinguish users.',
     expiry: '2 years',
     type: CookieType.HTTP,
-}
-
-export const GAT_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
-    name: '_gat',
-    provider: Provider.Google,
-    providerUrl: RELEVANT_LINKS.Google.privacy,
-    purpose: 'Used by Google Analytics to throttle request rate.',
-    expiry: '1 day',
-    type: CookieType.HTTP
 }
 
 export const GID_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
     name: '_gid',
     provider: Provider.Google,
     providerUrl: RELEVANT_LINKS.Google.privacy,
-    purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
-    expiry: '1 day',
+    purpose: 'Used to distinguish users.',
+    expiry: '24 hours',
+    type: CookieType.HTTP
+}
+
+export const GAT_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
+    name: '_gat',
+    provider: Provider.Google,
+    providerUrl: RELEVANT_LINKS.Google.privacy,
+    purpose: 'Used to throttle request rate. If Google Analytics is deployed via Google Tag Manager, this cookie will be named _dc_gtm_<property- id>.',
+    expiry: '1 minute',
+    type: CookieType.HTTP
+}
+
+export const AMP_TOKEN_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE: ServiceCookie = {
+    name: 'AMP_TOKEN',
+    provider: Provider.Google,
+    providerUrl: RELEVANT_LINKS.Google.privacy,
+    purpose: 'Contains a token that can be used to retrieve a Client ID from AMP Client ID service. Other possible values indicate opt-out, inflight request or an error retrieving a Client ID from AMP Client ID service.',
+    expiry: '30 seconds to 1 year',
     type: CookieType.HTTP
 }
 
@@ -83,8 +92,17 @@ export const GA_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
     name: '_ga',
     provider: Provider.Google,
     providerUrl: RELEVANT_LINKS.Google.privacy,
-    purpose: 'Registers a unique ID that is used to generate statistical data on how the visitor uses the website.',
+    purpose: 'Used to distinguish users.',
     expiry: '2 years',
+    type: CookieType.HTTP
+}
+
+export const GID_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
+    name: '_gid',
+    provider: Provider.Google,
+    providerUrl: RELEVANT_LINKS.Google.privacy,
+    purpose: 'Used to distinguish users.',
+    expiry: '24 hours',
     type: CookieType.HTTP
 }
 
@@ -97,16 +115,27 @@ export const GA_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
     type: CookieType.HTTP
 }
 
+export const GAC_GB_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE: ServiceCookie = {
+    name: '_gac_gb_<container-id>',
+    provider: Provider.Google,
+    providerUrl: RELEVANT_LINKS.Google.privacy,
+    purpose: ' 	Contains campaign related information. If you have linked your Google Analytics and Google Ads accounts, Google Ads website conversion tags will read this cookie unless you opt-out.',
+    expiry: '90 days',
+    type: CookieType.HTTP
+}
 
 export const GoogleOwnCookies = {
     GoogleAnalyticsUniversal: [
         GA_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE,
         GAT_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE,
         GID_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE,
-        GAC_PROPERTY_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE
+        GAC_PROPERTY_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE,
+        AMP_TOKEN_GOOGLE_ANALYTICS_UNIVERSAL_COOKIE
     ],
     GoogleAnalytics4: [
         GA_GOOGLE_ANALYTICS_4_COOKIE,
-        GA_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE
+        GID_GOOGLE_ANALYTICS_4_COOKIE,
+        GA_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE,
+        GAC_GB_CONTAINER_GOOGLE_ANALYTICS_4_COOKIE
     ]
 }
