9fd8d9f0a16f13d9fe9472d9df0cb5ec36ab6a0c
JC Dev <88061365+VmMad@users.noreply.github.com>
2022-05-06 16:09:46 +0200

    fix: additional cookies reactivity

--- src/lib/components/CookieLibrary/AdditionalCookies.svelte
+++ src/lib/components/CookieLibrary/AdditionalCookies.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { configuredServices } from '$lib/app/store';
 	import type { ServiceCookie } from '$lib/app/types';
-	import { get } from 'svelte/store';
 
 	let cookies: ServiceCookie[] = [];
 
-	onMount(() => {
-		//Google Universal and Google 4 have some duplicate cookies. Code below removes those duplicates
-		cookies = get(configuredServices).reduce((accumulator, service) => {
-			let cookiesName = accumulator.map((cookie) => cookie.name);
-			let serviceCookies = service.cookies.filter((cookie) => {
-				return !cookiesName.includes(cookie.name);
-			});
-			return accumulator.concat(serviceCookies);
-		}, []);
-	});
+	//Google Universal and Google 4 have some duplicate cookies. Code below removes those duplicates
+	$: cookies = $configuredServices.reduce((accumulator, service) => {
+		let cookiesName = accumulator.map((cookie) => cookie.name);
+		let serviceCookies = service.cookies.filter((cookie) => {
+			return !cookiesName.includes(cookie.name);
+		});
+		return accumulator.concat(serviceCookies);
+	}, []);
 </script>
 
 {#if cookies.length}
@@ -31,23 +27,18 @@
 		</thead>
 		<tbody>
 			{#each cookies as cookie}
-				{#if cookie && cookie.name}
-					<tr>
-						<td> {cookie?.name} </td>
-						<td
-							><a href={cookie?.providerUrl} target="_blank" rel="noopener noreferrer nofollow">
-								{cookie?.provider}</a
-							>
-						</td>
-						<td> {cookie?.purpose} </td>
-						<td> {cookie?.expiry} </td>
-						<td> {cookie?.type} </td>
-					</tr>
-				{/if}
+				<tr>
+					<td> {cookie?.name} </td>
+					<td
+						><a href={cookie?.providerUrl} target="_blank" rel="noopener noreferrer nofollow">
+							{cookie?.provider}</a
+						>
+					</td>
+					<td> {cookie?.purpose} </td>
+					<td> {cookie?.expiry} </td>
+					<td> {cookie?.type} </td>
+				</tr>
 			{/each}
 		</tbody>
 	</table>
 {/if}
-
-<style type="text/scss">
-</style>
