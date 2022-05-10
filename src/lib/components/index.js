91d4a9d37f2b3c8c0a64569ae12f94551292d464
JCDev <jcnoguera.dev@gmail.com>
2022-05-10 13:56:46 +0200

    Fix: Update styles import

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -4,3 +4,5 @@ export { default as CookieLibrary } from './CookieLibrary/index.svelte'
 export { default as CookieManager } from './CookieManager.svelte'
 export { default as Disclaimer } from './Disclaimer.svelte'
 export { default as NecessaryCookies } from './CookieLibrary/NecessaryCookies.svelte'
+
+import '../style/style.scss'
