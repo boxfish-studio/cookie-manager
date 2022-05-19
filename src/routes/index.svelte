1c778cbe37761c2725dda44a968a3d16ce2796e6
JC Dev <88061365+VmMad@users.noreply.github.com>
2022-05-19 12:22:40 +0200

    Fix: CSS theme variables not working (#34)
    * Fix: Update css variables
    
    * Fix CSS variables being undefined
    
    This merges defaults colors with user defined colors.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script context="module" lang="ts">
 	import { CookieLibrary } from '$components'
 	import type { SKCMConfiguration } from '$lib/app/types'
-	
+
 	export const prerender = true
 
 	let configuration: SKCMConfiguration = {
